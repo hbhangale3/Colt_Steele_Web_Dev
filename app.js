b9aad044faa44baebfbf7b838fd3df866b9e0d1b
hbhangale3 <hbhangale3@gmail.com>
2025-07-03 02:40:10 -0700

    exploring Conditionals JS

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ console.log("Hello from my first script");
 
 let userInput = prompt("Enter your fav number between 0 to 100");
 
-if(parseInt(userInput)>50){
-    console.log("Number Greater than 50");
-}else
-    console.log("Number less than 50");
+if(parseInt(userInput)%2===0){
+    console.log("even");
+}
