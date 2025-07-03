c0727d13fa6320192231a3afce82882c9a5df276
hbhangale3 <hbhangale3@gmail.com>
2025-07-03 02:34:01 -0700

    Conditionals JS

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 console.log("Hello from my first script");
-console.log("GoodBye");
+
+let userInput = prompt("Enter your fav number between 0 to 100");
+
+if(parseInt(userInput)>50){
+    console.log("Number Greater than 50");
+}else
+    console.log("Number less than 50");
