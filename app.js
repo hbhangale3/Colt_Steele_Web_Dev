24a4ec92c4f87799b75aea8095b624c0ccddd2eb
hbhangale3 <hbhangale3@gmail.com>
2025-07-03 10:15:55 -0700

    exploring Conditionals JS

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 console.log("Hello from my first script");
 
-let userInput = prompt("Enter your fav number between 0 to 100");
+let userInput = prompt("Enter your prompt");
 
-if(parseInt(userInput)%2===0){
-    console.log("even");
+// if(parseInt(userInput)%2===0){
+//     console.log("even");
+// }
+
+if(userInput=="stop"){
+    console.log("red");
+}else if(userInput=="slow"){
+    console.log("yellow");
+}else if(userInput=="go"){
+    console.log("green");
+}else{
+    console.log("purple");
 }
