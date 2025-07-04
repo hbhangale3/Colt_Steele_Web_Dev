62c054344e69882a0e3b50485f979c79714c3c72
hbhangale3 <hbhangale3@gmail.com>
2025-07-04 13:40:53 -0700

    exploring loops in JS

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 console.log("Hello from my first script");
 
-let userInput = prompt("Enter your number");
+// let userInput = prompt("Enter your number");
 
 // if(parseInt(userInput)%2===0){
 //     console.log("even");
 // }
 
-if(userInput=="stop"){
-    console.log("red");
-}else if(userInput=="slow"){
-    console.log("yellow");
-}else if(userInput=="go"){
-    console.log("green");
-}else{
-    console.log("purple");
-}
+// if(userInput=="stop"){
+//     console.log("red");
+// }else if(userInput=="slow"){
+//     console.log("yellow");
+// }else if(userInput=="go"){
+//     console.log("green");
+// }else{
+//     console.log("purple");
+// }
 
-for(let i=0;i<6;i++){
-    console.log("Da ba dee da ba daa");
-}
+// for(let i=0;i<6;i++){
+//     console.log("Da ba dee da ba daa");
+// }
     
 
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+
+for(let i=0;i<people.length;i++){
+    let string = people[i].toUpperCase();
+    console.log(string);
+}
