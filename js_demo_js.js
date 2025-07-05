c6aa992c38586ac642e2cc00a7dc5989d44d517c
hbhangale3 <hbhangale3@gmail.com>
2025-07-05 11:19:27 -0700

    functions in JS

--- js_demo_js.js
+++ js_demo_js.js
@@ -3,3 +3,17 @@
 const square = function(x){
     return x*x;
 }
+
+//higher order function
+function callTwice(func){
+    func();
+    func();
+}
+
+function rollDie(){
+    const roll = Math.floor(Math.random()*6)+1;
+    console.log(roll);
+}
+
+//we are not passing rollDie() but rollDie as otherwise it would first get executed and then pass the random number generated, rather than the function itself.
+callTwice(rollDie);
