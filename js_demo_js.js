44aad6d5544e42797a945914f653199a5c4f7db1
hbhangale3 <hbhangale3@gmail.com>
2025-07-05 11:45:24 -0700

    Methods in JS

--- js_demo_js.js
+++ js_demo_js.js
@@ -34,3 +34,15 @@ function mystryFunction(){
         }
     }
 }
+
+//defining own methods.
+
+const myMath = {
+    PI: 3.14159,
+    square: function(num){
+        return num*num;
+    },
+    cube: function(num){
+        return num**3;
+    }
+}
