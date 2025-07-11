b7c89282ef61b502a7c761ec9a07138c69833cfc
hbhangale3 <hbhangale3@gmail.com>
2025-07-11 06:39:09 -0700

    creating a button to get a new joke from API everytime its clicked

--- Async_JS_CODE/PromisesIntro/Axios_demo_app.js
+++ Async_JS_CODE/PromisesIntro/Axios_demo_app.js
@@ -1,4 +1,8 @@
 
+
+const jButton = document.querySelector('#jokeButton');
+const jList = document.querySelector('#jokes');
+
 const loadStarWarsData = async (id)=>{
     try{
         const res = await axios.get(`https://swapi.tech/api/people/${id}`);
@@ -10,3 +14,22 @@ const loadStarWarsData = async (id)=>{
 }
 
 loadStarWarsData(5);
+
+const dadJoke = async()=>{
+    try{
+        const config = {headers:{ Accept: 'application/json'}}
+        const res = await axios.get('https://icanhazdadjoke.com/', config);
+
+        const li = document.createElement('LI');
+        li.append(res.data.joke);
+        jList.append(li);
+        console.log(res.data);
+    }catch(e){
+        console.log("error in fetching jokes")
+    }
+}
+
+jButton.addEventListener('click', function(){
+    dadJoke();
+})
+
