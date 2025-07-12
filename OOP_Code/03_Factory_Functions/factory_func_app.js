bb08c0f0bdc9b585802ff23a3f40c09a9d8c20cb
hbhangale3 <hbhangale3@gmail.com>
2025-07-12 00:16:28 -0700

    Classes in JS

--- OOP_Code/03_Factory_Functions/factory_func_app.js
+++ OOP_Code/03_Factory_Functions/factory_func_app.js
@@ -1,25 +1,25 @@
-function makeColor(r,g,b){
-    const color = {};
-    color.r=r;
-    color.g=g;
-    color.b=b;
+// function makeColor(r,g,b){
+//     const color = {};
+//     color.r=r;
+//     color.g=g;
+//     color.b=b;
 
-    //using this here because in JS method captures the values of r, g, b from the outer function once and for all
-    //meaning they are tied to the variables at the time the method was created, not to the object itself.method captures the values of r, g, b from the outer function once and for all
-    color.rgb = function(){
-        const{r, g , b} = this;
-        return `rgb(${r}, ${g}, ${b})`
-    }
+//     //using this here because in JS method captures the values of r, g, b from the outer function once and for all
+//     //meaning they are tied to the variables at the time the method was created, not to the object itself.method captures the values of r, g, b from the outer function once and for all
+//     color.rgb = function(){
+//         const{r, g , b} = this;
+//         return `rgb(${r}, ${g}, ${b})`
+//     }
 
-    color.hex = function() {
-		const { r, g, b } = this;
-		return (
-			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
-		)
-	}
-//returning the color object.
-    return color;
-}
+//     color.hex = function() {
+// 		const { r, g, b } = this;
+// 		return (
+// 			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+// 		)
+// 	}
+// //returning the color object.
+//     return color;
+// }
 
 // const col1 = makeColor(240,67,180);
 // const col2 = makeColor(100,145,10);
@@ -40,3 +40,36 @@ Car.prototype.print = function(){
 
 const car1 = new Car("CR-V", "Honda", 2015);
 
+
+class Color{
+    constructor(r,g,b){
+        console.log("Inside Constructor");
+        this.r=r;
+        this.g=g;
+        this.b=b;
+    }
+
+    greet(){
+        console.log("Hello from the method");
+    }
+    innerRGB(){
+        const{r,g,b}=this;
+        return `${r}, ${g}, ${b}`;
+    }
+
+    rgb(){
+        return `rgb(${this.innerRGB()})`;
+    }
+
+    rgba(a=0.5){
+        return `rgba(${this.innerRGB},${a})`;
+    }
+
+    hex(){
+        return (
+            			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+            		)
+    }
+}
+
+const col3 = new Color(190,145,253);
