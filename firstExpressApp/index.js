a2011e13edcb458485baff03d556d6d603d95d4a
hbhangale3 <hbhangale3@gmail.com>
2025-07-20 11:36:04 -0700

    exploring res and req of use method

--- firstExpressApp/index.js
+++ firstExpressApp/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.use(()=>{
+app.use((req,res)=>{
     console.log("detected an incoming request");
+    res.send("We have received your request, thanks for reaching out to us.");
 })
 app.listen(3000,()=>{
     console.log("listening for incoming request");
