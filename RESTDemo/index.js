d5fd2cdc58516f5b3ba5df3c4103825dfd08a68c
hbhangale3 <hbhangale3@gmail.com>
2025-07-22 17:45:52 -0700

    accessing post body data

--- RESTDemo/index.js
+++ RESTDemo/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended:true}));
+
 app.get('/tacos',(req,res)=>{
     res.send("GET /tacos Response")
 })
 
 app.post('/tacos',(req,res)=>{
+    console.log(req.body);
     res.send("POST /tacos Response")    
+
 })
 
 app.listen(3000,()=>{
