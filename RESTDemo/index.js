896cb6ded4df4d15422b27ee6a4997256a1bdb14
hbhangale3 <hbhangale3@gmail.com>
2025-07-24 07:50:37 -0700

    Implementing CRUD functionality

--- RESTDemo/index.js
+++ RESTDemo/index.js
@@ -1,8 +1,96 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const {v4:uuid} = require('uuid');
+const methodOverride = require('method-override');
 
+app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended:true}));
+app.set('views', path.join(__dirname,'views'))
+app.set('view engine', 'ejs');
 
+let comments = [
+    {   
+        id: uuid(),
+        username: 'harsh',
+        comment: 'hey there'
+    },
+    {   
+        id:uuid(),
+        username: 'shivangi',
+        comment: 'dont make me angry'
+    },
+    {   
+        id:uuid(),
+        username: 'tod',
+        comment: 'Please delete my account, TOD'
+    }
+]
+//list all comments
+app.get('/comments',(req,res)=>{
+    res.render('index',{comments});
+})
+
+//share form with user
+app.get('/comments/new',(req,res)=>{
+    res.render('new');
+})
+
+//fetch user comment
+app.post('/comments', (req,res)=>{
+    console.log(req.body);
+    const {username,comment} = req.body;
+    comments.push({username,comment, id:uuid()});
+    //res.send("IT WORKED");
+    res.redirect('/comments');
+})
+
+//selecting a specific comment
+app.get('/comments/:id',(req,res)=>{
+    const {id} = req.params;
+    const comment = comments.find(c =>{
+        if(c.id===id)
+            return c
+    })
+
+    res.render('show', {comment});
+})
+
+//patching a specific comment
+app.patch('/comments/:id',(req,res)=>{
+    const {id } = req.params;
+    const newComment = req.body.comment;
+    console.log(req.body.comment)
+    const foundComment = comments.find(c =>{
+        if(c.id===id)
+            return c;
+    })
+    foundComment.comment=newComment;
+
+    res.redirect('/comments');
+})
+
+//editing an existing comment
+app.get('/comments/:id/edit',(req, res)=>{
+    const {id} = req.params;
+    const oldComment = comments.find(c =>{
+        if(c.id===id)
+            return c;
+    })
+//const oldCom = oldComment.comment;
+    res.render('edit',{oldComment});
+})
+
+//deleting a comment 
+app.delete('/comments/:id',(req,res)=>{
+    const {id} = req.params;
+    comments = comments.filter(c =>{
+        if(c.id!==id)
+            return c;
+    })
+
+    res.redirect('/comments')
+})
 app.get('/tacos',(req,res)=>{
     res.send("GET /tacos Response")
 })
